5f4e77b69d2ab1a32894b8b3e3c7146c0261dd27
moussor <etienne@moussor.com>
2017-02-23 11:06:14 +0100

    ajout flux audio training (+ fixes, it works)

--- client/components/training.js
+++ client/components/training.js
@@ -1,3 +1,5 @@
+var streamCheckInterval;
+
 Template.training.events({
 
   'click #cuppasInc': function(){
@@ -92,5 +94,211 @@ Template.training.onRendered(function () {
     $('#oui, #non, #euh').hide();
     $('#sacbouttons input, #sacbouttons button').hide();
   }
+
+
+
+  //streaming janus
+  var server = null;
+  // if(window.location.protocol === 'http:')
+  //     server = "http://" + window.location.hostname + ":8088/janus";
+  // else
+  //     server = "https://" + window.location.hostname + ":8089/janus";
+
+  // var host = window.location.hostname == "localhost" ? "www.on-appuiera-sur-espace-une-fois-rendu-a-la-page-d-accueil.com" : window.location.hostname;
+  // var host = window.location.hostname == "localhost" ? "www.on-appuiera-sur-espace-une-fois-rendu-a-la-page-d-accueil.com" : window.location.hostname;
+  var host = "www.on-appuiera-sur-espace-une-fois-rendu-a-la-page-d-accueil.com";
+  var server = null;
+  if(window.location.protocol === 'http:')
+      server = "http://" + host + ":8088/janus";
+  else
+      server = "https://" + host + ":8089/janus";
+
+  var janus = null;
+  // var streaming = null;
+  streaming = null;
+  var started = false;
+  var spinner = null;
+
+  var selectedStream = null;
+
+  // Initialize the library (all console debuggers enabled)
+  Janus.init({debug: "all", callback: function() {
+    
+    console.log('Janus initiated.');
+
+    // Use a button to start the demo
+    // $('#start').click(function() {
+    if(started)
+      return;
+    started = true;
+
+    // $(this).attr('disabled', true).unbind('click');
+    // Make sure the browser supports WebRTC
+    if(!Janus.isWebrtcSupported()) {
+      console.log("No WebRTC support... ");
+      return;
+    }
+    console.log('WebRTC is supported.');
+    console.log('Creating Janus Session...');
+    // Create session
+    janus = new Janus({
+      server: server,
+      success: function() {
+        // Attach to streaming plugin
+        janus.attach({
+          plugin: "janus.plugin.streaming",
+          success: function(pluginHandle) {
+            // $('#details').remove();
+            streaming = pluginHandle;
+            Janus.log("Plugin attached! (" + streaming.getPlugin() + ", id=" + streaming.getId() + ")");
+            console.log("Plugin attached! (" + streaming.getPlugin() + ", id=" + streaming.getId() + ")");
+            // Setup streaming session
+            // $('#update-streams').click(updateStreamsList);
+            // updateStreamsList();
+            //janus.destroy();
+              //si le pouvoir est déjà aux mains de l'admin lancons le stream au chargement de la page
+              // var powerToThePeople = superGlobals.findOne({ powerToThePeople: { $exists: true}});
+              // var isPowerToThePeople = (powerToThePeople) ? powerToThePeople.powerToThePeople : true;
+              // var isPowerToThePeople = getSuperGlobal("powerToThePeople", false); // par défaut false, admin a le pouvoir -> donc lancer le stream (?)
+              // if(!isPowerToThePeople) {
+                // console.log("le pouvoir est déjà aux mains de l'admin lancons le stream au chargement de la page");
+                startTheStream();
+              // }
+
+          },
+          error: function(error) {
+            Janus.error("  -- Error attaching plugin... ", error);
+            console.log("Error attaching plugin... " + error);
+          },
+          onmessage: function(msg, jsep) {
+            Janus.debug(" ::: Got a message :::");
+            Janus.debug(JSON.stringify(msg));
+            var result = msg["result"];
+            if(result !== null && result !== undefined) {
+              if(result["status"] !== undefined && result["status"] !== null) {
+                var status = result["status"];
+                if(status === 'starting')
+                  console.log("Message : Starting, please wait...");
+                  // $('#status').removeClass('hide').text("Starting, please wait...").show();
+                else if(status === 'started')
+                  console.log("Message : Started");
+                  // $('#status').removeClass('hide').text("Started").show();
+                else if(status === 'stopped')
+                  stopStream();
+              }
+            } else if(msg["error"] !== undefined && msg["error"] !== null) {
+              console.log("Message : "+msg["error"]);
+              stopStream();
+              return;
+            }
+            if(jsep !== undefined && jsep !== null) {
+              Janus.debug("Handling SDP as well...");
+              Janus.debug(jsep);
+              // Answer
+              streaming.createAnswer({
+                jsep: jsep,
+                media: { audioSend: false, videoSend: false },  // We want recvonly audio/video
+                success: function(jsep) {
+                  Janus.debug("Got SDP!");
+                  Janus.debug(jsep);
+                  var body = { "request": "start" };
+                  streaming.send({"message": body, "jsep": jsep});
+                  // $('#watch').html("Stop").removeAttr('disabled').click(stopStream);
+                },
+                error: function(error) {
+                  Janus.error("WebRTC error:", error);
+                  console.log("WebRTC error... " + JSON.stringify(error));
+                }
+              });
+            }
+          },
+          onremotestream: function(stream) {
+            Janus.debug(" ::: Got a remote stream :::");
+            console.log(" ::: Got a remote stream :::");
+            console.log(JSON.stringify(stream));
+            Janus.debug(JSON.stringify(stream));
+            // if($('#remotevideo').length === 0)
+            //   $('#stream').append('<video class="rounded centered hide" id="remotevideo" width=320 height=240 autoplay/>');
+            // Show the stream and hide the spinner when we get a playing event
+            // $("#remotevideo").bind("playing", function () {
+            //   $('#waitingvideo').remove();
+            //   $('#remotevideo').removeClass('hide');
+            //   if(spinner !== null && spinner !== undefined)
+            //     spinner.stop();
+            //   spinner = null;
+            // });
+            // Janus.attachMediaStream($('#remotevideo').get(0), stream);
+            console.log($('#stream-video'), $('#stream-video').get(0));
+            Janus.attachMediaStream($('#stream-video').get(0), stream);
+          },
+          oncleanup: function() {
+            Janus.log(" ::: Got a cleanup notification :::");
+            // $('#waitingvideo').remove();
+            // $('#remotevideo').remove();
+          }
+        });
+      },
+      error: function(error) {
+        Janus.error(error);
+        console.log(error);
+        // bootbox.alert(error, function() {
+        //   window.location.reload();
+        // });
+      },
+      destroyed: function() {
+        console.log("destroyed");
+        window.location.reload();
+      }
+    });
+  }});
+
+
+
+  function startTheStream(what) {
+
+    console.log('salm startstream!', what, streaming);
+
+    var body = { "request": "watch", id: parseInt(1) };
+    streaming.send({"message": body});
+
+    if (!streamCheckInterval) {
+        streamCheckInterval = setInterval(function(){checkTheStream();}, 30000); 
+    }
+    // if($('#streamFrame').length == 0) {
+
+    //   $('<iframe>', {
+    //   'src': 'http://www.on-appuiera-sur-espace-une-fois-rendu-a-la-page-d-accueil.com/stream/',
+    //    id:  'streamFrame',
+    //    frameborder: 0,
+    //    scrolling: 'no'
+    //   }).appendTo('#stream-ifr');
+
+
+    //   setTimeout(function(){
+    //     console.log('streamFrame', $("#streamFrame").contents().find('#start'));
+    //     var startButton = $("#streamFrame").contents().find('#start');
+    //     if(startButton.length > 0) {
+    //       startButton.trigger('click');
+    //     }
+    //   }, 2000);
+    //   // $('streamFrame').on('load', function(){
+    //   //   console.log('streamFrame loaded');
+    //   // });
+    //   // console.log('streamFrame added');
+    //   // $('streamFrame').attr();
+    //   // console.log('streamFrame src', $('streamFrame').attr('src'));
+    // }
+
+
+
+  }
+
+  function checkTheStream(){
+    console.log('checkTheStream!');
+    if(!Janus.initDone || !streaming) Janus.init();
+    if(!streaming.webrtcStuff.started) startTheStream();
+  }
+
+
 
 });
